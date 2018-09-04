99fe9455e5ca3f40c8bd5dd6f43201452d74d673
matt <matt@automaton.uk>
2018-09-04 11:06:21 -0400

    Add validation to submitted data

--- api/src/main/java/smallproject/handler/RegistrationHandler.java
+++ api/src/main/java/smallproject/handler/RegistrationHandler.java
@@ -1,11 +1,7 @@
 package smallproject.handler;
 
-import com.google.common.io.CharStreams;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import smallproject.dao.SessionDao;
@@ -16,16 +12,25 @@ import smallproject.model.User;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 /**
  * @author Matthew
  */
 public class RegistrationHandler extends AbstractHandler {
 
+    private static final Predicate<String> PATTERN_EMAIL = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\." +
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$").asPredicate();
+
     private static final String ERROR_DESERIALIZE_FAIL = "unable to construct user from payload";
+    private static final String ERROR_INVALID_EMAIL = "invalid email address";
+    private static final String ERROR_INVALID_PASSWORD = "invalid password";
+    private static final String ERROR_INVALID_FIRST_NAME = "invalid first name";
+    private static final String ERROR_INVALID_LAST_NAME = "invalid last name";
 
     public RegistrationHandler(final Jdbi dbi) {
         super(dbi);
@@ -45,28 +50,50 @@ public class RegistrationHandler extends AbstractHandler {
         // get the IP that the request came from
         final String ip = this.getIpAddress(req);
 
-        final User user = new Gson().fromJson(getPayload(req.getReader()), User.class);
-        if (user == null || user.email == null || user.firstname == null || user.lastname == null) {
-            log.warning("Registration failed for user: " + user);
-            // there was an issue with the JSON payload, display error
+        // take the payload and deserialize it to a User
+        final User registrant = new Gson().fromJson(getPayload(req.getReader()), User.class);
+        if (registrant == null) {
+            // if the object is null that means deserialization failed... return error
             error(response, ERROR_DESERIALIZE_FAIL);
-            req.getReader().close();
             return;
         }
 
+        // validate email address
+        if (registrant.email == null || registrant.email.isEmpty() || !PATTERN_EMAIL.test(registrant.email)) {
+            error(response, ERROR_INVALID_EMAIL);
+            return;
+        }
+
+        // validate password
+        if (registrant.password == null || registrant.password.isEmpty()) {
+            error(response, ERROR_INVALID_PASSWORD);
+            return;
+        }
+
+        // validate first name
+        if (registrant.firstname == null || registrant.firstname.isEmpty()) {
+            error(response, ERROR_INVALID_FIRST_NAME);
+            return;
+        }
+
+        // validate last name
+        if (registrant.lastname == null || registrant.lastname.isEmpty()) {
+            error(response, ERROR_INVALID_LAST_NAME);
+            return;
+        }
 
         // open connection to the database
         try (final Handle h = dbi.open()) {
             // attach the handle to the userdao
             UserDao dao = h.attach(UserDao.class);
             // look up the email in the database
-            final User registeredUser = dao.getUserByEmail(user.email);
+            final User registeredUser = dao.getUserByEmail(registrant.email);
             if (registeredUser != null && registeredUser.id != -1) {
                 // user already exists, lets see if the password is correct...
-                final User authenticatedUser = dao.userLogin(user.email, user.password);
+                final User authenticatedUser = dao.userLogin(registrant.email, registrant.password);
                 if (authenticatedUser == null) {
                     // email exists in the database, but password was wrong, return an error
-                    log.warning("Registration failed for user: \"" + user.email + "\", user already exists!");
+                    log.warning("Registration failed for user: \"" + registrant.email + "\", user already exists!");
                     error(response, "user already exists!");
                 } else {
                     // user email already exists... but password is correct, so let's just log them in
@@ -75,13 +102,13 @@ public class RegistrationHandler extends AbstractHandler {
                     sendSession(response, session);
                 }
             } else {
-                dao.insert(user);
+                long id = dao.insert(registrant);
+                registrant.setId(id);
                 final SessionDao sessionDao = h.attach(SessionDao.class);
-                final Session session = sessionDao.create(user, ip);
+                final Session session = sessionDao.create(registrant, ip);
                 sendSession(response, session);
             }
         }
-
         req.getReader().close();
     }
 

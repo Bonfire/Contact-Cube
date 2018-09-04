3a6c3d7111d14c74d421b097ba891f15e3a196f0
matt <matt@automaton.uk>
2018-09-04 11:04:55 -0400

    Change userId to long

--- api/src/main/java/smallproject/model/User.java
+++ api/src/main/java/smallproject/model/User.java
@@ -9,17 +9,17 @@ import com.google.gson.annotations.SerializedName;
  */
 public class User {
 
-    public int id = -1;
+    public long id = -1;
     public String email;        // email/username of the user
     public String password;     // password HASH of the user
     public String firstname;    // first name of the user
     public String lastname;     // last name of the user
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
